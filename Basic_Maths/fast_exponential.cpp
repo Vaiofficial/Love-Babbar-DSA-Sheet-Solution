ee8fd28121889691548e29575acd617466d5f255
P Vaibhav <pvaibhavofficial@gmail.com>
2023-10-09 22:01:28 +0530

    basic maths

---
+++ Basic_Maths/fast_exponential.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int powerBasic(int a, int b)
+{
+    int ans = 1;
+    for (int i = 0; i < b; i++)
+    {
+        ans = ans * a;
+    }
+    return ans;
+}
+
+int powerFast(int a , int b)
+{
+    int ans = 1;
+    while(b>0)
+    {
+        if(b&1)
+        {
+            ans  = ans*a;
+        }
+        a = a * a;
+        b>=1;
+    }
+    return ans;  
+}
+
+int main()
+{
+    cout <<powerBasic(2, 3) << endl;
+    cout <<powerFast(2, 3) << endl;
+
+    return 0;
+}
